fe1eea71f8bc7ec4d8ef212d2e4f8faa2d2cfc21
surya1811 <70018056+surya1811@users.noreply.github.com>
2021-12-05 00:11:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { createContext, useReducer } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./components/Home";
 import Login from "./components/Login";
 import Githubs from "./components/Githubs";
 import Details from "./components/Details";
